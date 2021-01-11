47a36e49b6e2fa9abbb88e99d2cc6edc01fa1952
Puleeno Nguyen <puleeno@gmail.com>
2021-01-11 13:49:42 +0700

    Create plugin file

---
+++ lord-seo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Lord SEO
+ * Plugin URI: https://code.puleeno.com/
+ * Version: 0.0.1
+ * Description: Another WordPress SEO plugin
+ * Author: Puleeno Nguyen
+ * Author URI: https://puleeno.com
+ * Tag: SEO
+ */
+
